f1ea1a74be64f24276261502fe085ad063a71d30
Teemu Parviainen <parviainen.teemu@gmail.com>
2019-10-27 19:04:45 +0200

    Added Rolling and Overlay screeneffects

--- ledtrix/effects/screeneffects.py
+++ ledtrix/effects/screeneffects.py
@@ -3,7 +3,23 @@ import numpy as np
 from PIL import Image
 from ledtrix.effects import Effect
 from ledtrix.effects.coloreffects import effect_complemetary_colors
-from ledtrix.helpers import rotate_image
+from ledtrix.helpers import rotate_image, reshape_image_array
+
+class EffectOverlay(Effect):
+    def __init__(self, screen_other, alpha=0, triggers=None):
+        super(EffectOverlay, self).__init__(triggers=triggers)
+        self.screen_other = screen_other
+        self.alpha = alpha
+
+    def initialize(self):
+        pass
+
+    def process(self, screen):
+        self.process_triggers()
+        alpha = self.coefficient * self.alpha
+        cropped_other = reshape_image_array(self.screen_other.pixel, size=(screen.width, screen.height), origin=(0,0))
+        screen.pixel = (1-alpha) * screen.pixel + alpha * cropped_other
+
 
 class EffectChangeBrighness(Effect):
     def __init__(self, brightness, triggers=None):
@@ -78,9 +94,10 @@ class EffectComplementaryColor():
 
     
 class EffectRotate(Effect):
-    def __init__(self, speed, triggers=None):
+    def __init__(self, speed, pivot=None, triggers=None):
         super(EffectRotate, self).__init__(triggers=triggers)
         self.speed = speed
+        self.pivot = pivot
         # Initialize angle
         self.angle = 0
 
@@ -96,11 +113,36 @@ class EffectRotate(Effect):
         self.angle += self.direction * self.speed
         # center pivot
         x, y, _ = pixel_array.shape
-        pivot_x = int(x/2)
-        pivot_y = int(y/2)
-        rotated = rotate_image(pixel_array, self.angle, np.array([pivot_x,pivot_y]))
+        if self.pivot is None:
+            pivot_x = int(x/2)
+            pivot_y = int(y/2)
+            pivot = [pivot_x, pivot_y]
+        else:
+            pivot = self.pivot
+        rotated = rotate_image(pixel_array, self.angle, np.array(pivot))
         # Debug
         # Image.fromarray(rotated.astype(np.uint8)).save('{}.png'.format(self.angle))
         screen.pixel = rotated
     
 
+class EffectRoll(Effect):
+    def __init__(self, axis, shift):
+        # Use last frame as a reference
+        super().__init__()
+        self.axis = axis
+        self.shift = shift
+        # Initialize
+        self.shift_x = 0
+        self.shift_y = 0
+        self.shift_z = 0
+
+    def initialize(self):
+        self.shift_x = 0
+        self.shift_y = 0
+        self.shift_z = 0
+
+    def process(self, screen):
+        self.shift_x += self.shift[0]
+        self.shift_y += self.shift[1]
+        self.shift_z += self.shift[2]
+        screen.pixel = np.roll(screen.pixel, axis=self.axis, shift=[self.shift_x, self.shift_y, self.shift_z])
